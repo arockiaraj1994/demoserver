99858488a71950be693d3665eaf7e4340ce7ab21
Arockiaraj Rayappan <arockiaraj@batonsystems.com>
2020-06-15 15:30:32 +0530

    Simple demo server added with health and jacoco report

---
+++ src/test/java/com/learning/demoserver/DemoserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learning.demoserver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	    assertTrue(true);
+	}
+
+}
