64d67cea590d257c7fd3df20dfe6bd33392a2864
Arockiaraj Rayappan <arockiaraj@batonsystems.com>
2020-06-16 19:10:47 +0530

    Added metrics

--- src/main/java/com/learning/demoserver/DemoserverApplication.java
+++ src/main/java/com/learning/demoserver/DemoserverApplication.java
@@ -1,13 +1,25 @@
 package com.learning.demoserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoserverApplication {
+    
+    Logger log = LoggerFactory.getLogger(DemoserverApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoserverApplication.class, args);
 	}
-
+	
+	@GetMapping(path = "/demoserver/ping")
+	public String ping() {
+	    log.info("Ping called");
+	    return "Ok";
+	}
 }
